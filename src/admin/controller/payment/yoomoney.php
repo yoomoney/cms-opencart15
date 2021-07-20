c16f1fee80363490dd224aa75d6b939871020959
Антон Н. Николаев <nikolaevanym@yoomoney.ru>
2021-07-20 15:11:15 +0300

    Заменён файл верификации для ApplePay. Обновлен SDK до версии 2.1.2

--- src/admin/controller/payment/yoomoney.php
+++ src/admin/controller/payment/yoomoney.php
@@ -26,7 +26,7 @@ class ControllerPaymentYoomoney extends Controller
     /**
      * @var string
      */
-    private $moduleVersion = '2.1.2';
+    private $moduleVersion = '2.1.3';
 
     /**
      * @var integer
