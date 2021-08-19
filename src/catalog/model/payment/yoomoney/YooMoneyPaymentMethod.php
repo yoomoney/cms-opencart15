7380a1620e88ac980f4b5b79ca713c51fdcad9ec
Aleksandra Spiridonova <sashaspi@yoomoney.ru>
2021-08-19 17:50:23 +0300

    Исправление ссылки на демо-магазин в readme

--- src/catalog/model/payment/yoomoney/YooMoneyPaymentMethod.php
+++ src/catalog/model/payment/yoomoney/YooMoneyPaymentMethod.php
@@ -5,7 +5,7 @@ use YooKassa\Model\PaymentMethodType;
 class YooMoneyPaymentMethod
 {
     /** @const string */
-    const MODULE_VERSION = '2.1.4';
+    const MODULE_VERSION = '2.1.5';
 
     /** @const string */
     const CUSTOM_PAYMENT_METHOD_WIDGET = 'widget';
